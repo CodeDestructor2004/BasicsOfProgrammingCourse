e9c50f6cffea110a42bd87fdeb132148407e6052
Null4ik <010programmist010@gmail.com>
2024-05-05 16:40:57 +0300

    Files task (2)

--- libs/data_structures/files/files.c
+++ libs/data_structures/files/files.c
@@ -50,7 +50,51 @@ void test_task_19_1() {
         "task_1.txt";
     const char *str_2 =
         "task_1_ref.txt";
-    int answer = 0;
+    int answer = 1;
+    if (!assert_txt(str_1, str_2))
+        answer = task_19_1(str_1);
+    printf("%d\n", assert_txt(str_1, str_2));
+}
+
+// В текстовом файле записаны вещественные числа в форме с
+// фиксированной точкой. Преобразовать файл, представив каждое
+// число в форме с плавающей точкой, сохранив две цифры после точки
+// в мантиссе.
+int task_19_2(const char *str) {
+    FILE *input_file = fopen(str, "r");
+    if (input_file == NULL) {
+        printf("Error: input file not found\n");
+        return 1;
+    }
+    
+    FILE *output_file = fopen("buffer_file.txt", "w");
+    if (output_file == NULL) {
+        printf("Error: output file not created\n");
+        return 1;
+    }
+
+    double number;
+
+    while (fscanf(input_file, "%lf", &number) == 1) {
+        fprintf(output_file, "%.2f ", number);
+    }
+
+    fclose(input_file);
+    fclose(output_file);
+
+    copyFileContent("buffer_file.txt",
+    str);
+    return 0;
+}
+
+
+void test_task_19_2() {
+    printf("test_task_19_2 - ");
+    const char *str_1 =
+        "task_2.txt";
+    const char *str_2 =
+        "task_2_ref.txt";
+    int answer = 1;
     if (!assert_txt(str_1, str_2))
         answer = task_19_1(str_1);
     printf("%d\n", assert_txt(str_1, str_2));
@@ -59,6 +103,7 @@ void test_task_19_1() {
 
 void tests() {
     test_task_19_1();
+    test_task_19_2();
 }
 
 
---
+++ libs/data_structures/files/tempCodeRunnerFile.c
@@ -0,0 +1,2 @@
+
+int task_19_2(const char *str) {
