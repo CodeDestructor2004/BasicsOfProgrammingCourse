0a6bb5600b1a300456ffd038c356bb83b3148f20
Null4ik <010programmist010@gmail.com>
2024-05-15 22:48:10 +0300

    Stream Task (3)

--- libs/data_structures/stream/stream.c
+++ libs/data_structures/stream/stream.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <assert.h>
 #include <stdbool.h>
+#include <malloc.h>
 #include "..\matrix\matrix.c"
 #include "..\files\file_lib.c"
 
@@ -122,7 +123,15 @@ void test_fill_matrix() {
     test_fill_matrix_3_more_matrix_element();
 }
 
-
+// . Согласно статье в Википедии : « Игра жизни» , также известная просто 
+// как «Жизнь », представляет собой клеточный автомат, изобретенный 
+// британским математиком Джоном Хортоном Конвеем в 1970 году.
+// Доска состоит из m x nсетки ячеек, где каждая ячейка имеет 
+// начальное состояние: живое (обозначается значком 1) или 
+// мертвое (обозначается значком 0). Каждая ячейка 
+// взаимодействует со своими восемью соседями 
+// (горизонтальными, вертикальными, диагональными), используя 
+// следующие четыре правила
 int is_point_alive(matrix *m, int i, int j) {
     bool is_alive = m->values[i][j];
 
@@ -287,9 +296,171 @@ void test_game_life() {
 }
 
 
+// Медианный фильтр — это метод обработки цифровых изображений, 
+// который используется для уменьшения шума на изображении. Для 
+// применения медианного фильтра к массиву, необходимо выполнить 
+// следующие шаги
+int cmp(const void* a, const void* b) {
+    return *(const unsigned int *) a - *(const unsigned int *) b;
+}
+
+
+int get_median_in_area(matrix* m, const int i, const int j, const int filter) {
+    int border = filter * 2 + 1;
+    int *temp = (int *) malloc((border * border) * sizeof(int));
+    int size = 0;
+
+    for (int row_i = i - filter; row_i <= i + filter; row_i++)
+        for (int col_j = j - filter; col_j <= j + filter; col_j++)
+            if (row_i != i || col_j != j) {
+                temp[size] = m->values[row_i][col_j];
+                size++;
+            }
+
+    qsort(temp, size, sizeof(int), cmp);
+    int result = (temp[size / 2 - 1] + temp[size / 2]) / 2;
+
+    free(temp);
+    return result;
+}
+
+
+void median_filter(const char* filename) {
+    FILE* file = f_safetyOpen(filename, "rb");
+
+    int n, filter;
+    fread(&n, sizeof(int), 1, file);
+    fread(&filter, sizeof(int), 1, file);
+
+    filter /= 2;
+
+    matrix m = getMemMatrix(n, n);
+    for (int i = 0; i < n; i++)
+        for (int j = 0; j < n; j++)
+            fread(&m.values[i][j], sizeof(int), 1, file);
+
+    for (int i = filter; i < n - filter; i++)
+        for (int j = filter; j < n - filter; j++)
+            m.values[i][j] = get_median_in_area(&m, i, j, filter);
+
+    fclose(file);
+
+    file = f_safetyOpen(filename, "wb");
+    filter = filter * 2 + 1;
+    fwrite(&n, sizeof(int), 1, file);
+    fwrite(&filter, sizeof(int), 1, file);
+    for (int i = 0; i < n; i++)
+        for (int j = 0; j < n; j++)
+            fwrite(&m.values[i][j], sizeof(int), 1, file);
+
+    freeMemMatrix(&m);
+    fclose(file);
+}
+
+
+void test_median_filter_1_empty_file() {
+    const char filename[] = "test_files/task_3_test_1.txt";
+    FILE* file = f_safetyOpen(filename, "wb");
+
+    int n = 0;
+    int filter = 7;
+    fwrite(&n, sizeof(int), 1, file);
+    fwrite(&filter, sizeof(int), 1, file);
+    fclose(file);
+
+    median_filter(filename);
+    file = f_safetyOpen(filename, "rb");
+    fread(&n, sizeof(int), 1, file);
+    fread(&filter, sizeof(int), 1, file);
+
+    fclose(file);
+
+    assert(n == 0);
+    assert(filter == 7);
+}
+
+
+void test_median_filter_2_unit_file() {
+    const char filename[] = "test_files/task_3_test_2.txt";
+    FILE* file = f_safetyOpen(filename, "wb");
+
+    int n = 1;
+    int x = 10;
+    int filter = 7;
+    fwrite(&n, sizeof(int), 1, file);
+    fwrite(&filter, sizeof(int), 1, file);
+    fwrite(&x, sizeof(int), 1, file);
+    fclose(file);
+
+    median_filter(filename);
+    file = f_safetyOpen(filename, "rb");
+    fread(&n, sizeof(int), 1, file);
+    fread(&filter, sizeof(int), 1, file);
+    fread(&x, sizeof(int), 1, file);
+
+    fclose(file);
+
+    assert(n == 1);
+    assert(filter == 7);
+    assert(x == 10);
+}
+
+
+void test_median_filter_3_average_matrix() {
+    const char filename[] = "test_files/task_3_test_3.txt";
+    FILE* file = f_safetyOpen(filename, "wb");
+
+    int n = 5;
+    int filter = 5;
+    fwrite(&n, sizeof(int), 1, file);
+    fwrite(&filter, sizeof(int), 1, file);
+    matrix mat = createMatrixFromArray((int[]) {10, 20, 30, 40, 50,
+                                                15, 24, 16, 17, 56,
+                                                34, 54, 12, 56, 78,
+                                                14, 45, 34, 47, 56,
+                                                17, 15, 24, 56, 62}, n, n);
+    for (int i = 0; i < n; i++)
+        for (int j = 0; j < n; j++)
+            fwrite(&mat.values[i][j], sizeof(int), 1, file);
+
+    fclose(file);
+    median_filter(filename);
+
+    file = f_safetyOpen(filename, "rb");
+    fread(&n, sizeof(int), 1, file);
+    fread(&filter, sizeof(int), 1, file);
+    matrix res = getMemMatrix(n, n);
+    for (int i = 0; i < n; i++)
+        for (int j = 0; j < n; j++)
+            fread(&res.values[i][j], sizeof(int), 1, file);
+    matrix check = createMatrixFromArray((int[]) {10, 20, 30, 40, 50,
+                                                15, 24, 16, 17, 56,
+                                                34, 54, 34, 56, 78,
+                                                14, 45, 34, 47, 56,
+                                                17, 15, 24, 56, 62}, n, n);
+
+    fclose(file);
+
+    assert(n == 5);
+    assert(filter == 5);
+    assert(areTwoMatricesEqual(&res, &check));
+
+    freeMemMatrix(&mat);
+    freeMemMatrix(&res);
+    freeMemMatrix(&check);
+}
+
+
+void test_median_filter() {
+    test_median_filter_1_empty_file();
+    test_median_filter_2_unit_file();
+    test_median_filter_3_average_matrix();
+}
+
 void tests() {
     test_fill_matrix();
     test_game_life();
+    test_median_filter();
 }
 
 
